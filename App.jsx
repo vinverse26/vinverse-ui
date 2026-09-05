619271cde3d78f5c550e51767bedfb9bb2890ed2
vinverse26 <architect@vinverse.ai>
2026-09-05 10:43:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="card">
+      <h1>Hello, World!</h1>
+      <p>Deployed via AWS Amplify + GitHub Actions</p>
+    </div>
+  )
+}
+
+export default App
